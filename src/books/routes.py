cd7530a873ec08ad8947353a98f9c1fc8b1e3054
Jay Rathod <jay.rathod@techabbot.com>
2026-02-05 17:53:59 +0530

    mend

--- src/books/routes.py
+++ src/books/routes.py
@@ -15,7 +15,7 @@ access_token_bearer = AccessTokenBearer()
 
 
 @book_router.post("/",response_model= Book,status_code=status.HTTP_201_CREATED)
-async def create_book(new_book: BookCreate, session: AsyncSession = Depends(get_session)): 
+async def create_book(new_book: BookCreate, session: AsyncSession = Depends(get_session),user_details=Depends(access_token_bearer)): 
     new_book = await book_service.create_book(new_book,session)
     return new_book
 
@@ -26,24 +26,24 @@ async def get_all_books(session: AsyncSession = Depends(get_session),user_detail
     book = await book_service.get_all_books(session)
     return book
 
-@book_router.get("/{book_id}",response_model=Book,dependencies=[Depends(access_token_bearer)])
-async def get_book_by_id(book_id : str, session: AsyncSession = Depends(get_session))-> dict:
+@book_router.get("/{book_id}",response_model=Book)
+async def get_book_by_id(book_id : str, session: AsyncSession = Depends(get_session),user_details=Depends(access_token_bearer))-> dict:
     book = await book_service.get_book_by_id(book_id,session)
     if book:
         return book
     else:
         raise HTTPException(status_code=404, detail=f"Book with id {book_id} not found")
 
-@book_router.patch("/{book_id}",status_code=status.HTTP_201_CREATED,response_model=Book,dependencies=[Depends(access_token_bearer)])
-async def update_book(book_id : str, book_data: BookUpdate, session: AsyncSession = Depends(get_session))-> dict:
+@book_router.patch("/{book_id}",status_code=status.HTTP_201_CREATED,response_model=Book)
+async def update_book(book_id : str, book_data: BookUpdate, session: AsyncSession = Depends(get_session),user_details=Depends(access_token_bearer))-> dict:
     updated_book = await book_service.update_book(book_id,book_data,session)
     if updated_book:
         return updated_book
     else:
         raise HTTPException(status_code=404, detail=f"Book with id {book_id} not found")
 
-@book_router.delete("/{book_id}",dependencies=[Depends(access_token_bearer)])
-async def delete_book(book_id : str, session: AsyncSession = Depends(get_session))-> dict:
+@book_router.delete("/{book_id}")
+async def delete_book(book_id : str, session: AsyncSession = Depends(get_session),user_details=Depends(access_token_bearer))-> dict:
     deleted_book = await book_service.delete_book(book_id,session)
     if deleted_book:
         return {"message": "Book deleted successfully"}
